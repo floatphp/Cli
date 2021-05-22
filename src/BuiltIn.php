1bb5440398fa50bdc8a0371e1a16fee80271008e
Jakiboy <j.sinnaour.official@gmail.com>
2021-05-22 09:58:26 +0100

    Update

--- src/BuiltIn.php
+++ src/BuiltIn.php
@@ -2,8 +2,8 @@
 /**
  * @author    : JIHAD SINNAOUR
  * @package   : FloatPHP
- * @subpackage: cli Component
- * @version   : 1.1.0
+ * @subpackage: CLI Component
+ * @version   : 1.0.0
  * @category  : PHP framework
  * @copyright : (c) 2017 - 2021 JIHAD SINNAOUR <mail@jihadsinnaour.com>
  * @link      : https://www.floatphp.com
